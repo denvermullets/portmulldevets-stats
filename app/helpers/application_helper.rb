4db83200e584f5911a86b59defceadba2f0fb3f8
Ryan V <47340962+denvermullets@users.noreply.github.com>
2024-05-26 07:30:50 -0400

    feat(dashboard): add pagy turbo and custom css for pagy nav (#14)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def turbo_pagy_nav(pagy, start_date, end_date)
+    render partial: 'shared/pagy_nav', locals: { pagy:, start_date:, end_date: }
+  end
 end
