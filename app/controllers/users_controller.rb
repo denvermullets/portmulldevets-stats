9e9d752fb521c0d99c1e98d1c36de24d6ecc323f
denvermullets <devmullets@gmail.com>
2024-05-27 09:28:00 -0400

    feat(users): add index view, update links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
+  include Pagy::Backend
+
   def index
-    users = User.joins(:events)
-                .where(created_at: 30.days.ago..Time.now)
-                .distinct
+    @start_date = params[:start_date] ? Date.parse(params[:start_date]) : 30.days.ago.to_date
+    @end_date = params[:end_date] ? Date.parse(params[:end_date]) : Date.today
+
+    @pagy, users = pagy(User.where(created_at: @start_date..@end_date))
 
     respond_to do |format|
       format.html { render :index, locals: { users: } }
       format.json { render json: users }
     end
+  end
+
+  def show
   end
 end
